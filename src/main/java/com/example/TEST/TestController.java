091aae5df035afe9ca3c689e3a14329c765e01b3
adamwillerton <131967086+adamwillerton@users.noreply.github.com>
2024-10-15 10:46:02 +0100

    This is version 1 of my TEST API

---
+++ src/main/java/com/example/TEST/TestController.java
@@ -0,0 +1,14 @@
+package com.example.TEST;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    //endpoint
+    @RequestMapping("/test")
+    public String getNumber() {
+        return "44444444";
+    }
+}
