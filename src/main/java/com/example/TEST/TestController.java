44a58065adb5b4d8faa7fa91fbc02fc036ac23d0
adamwillerton <131967086+adamwillerton@users.noreply.github.com>
2024-10-15 11:25:59 +0100

    Update TestController.java
    changed to 88888

--- src/main/java/com/example/TEST/TestController.java
+++ src/main/java/com/example/TEST/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
     //endpoint
     @RequestMapping("/test")
     public String getNumber() {
-        return "5555555555555";
+        return "8888888888888888";
     }
 }
